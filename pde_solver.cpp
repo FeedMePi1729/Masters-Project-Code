c39cd77aecb79bb378dc74244cfdac4506f03689
Pradyumna Harish <harishpradyumna@gmail.com>
2023-09-14 19:48:08 +0400

    Made a way to make the solution grid

--- pde_solver.cpp
+++ pde_solver.cpp
@@ -2,19 +2,25 @@
 
 
 void incrementSetter(double& timeIncrement, double& spaceIncrement){
+    // Used for setting the time and space increments
     double newTimeIncrement, newSpaceIncrement;
-
     std::cout << "Enter New Time Increment: "; std::cin >> newTimeIncrement;
     std::cout << "Enter New Space Increment: "; std::cin >> newSpaceIncrement;
     timeIncrement = newTimeIncrement;
     spaceIncrement = newSpaceIncrement;
 }
 
+ // TODO: find a way to make the initial condition of the grid be what we want it to be
+ 
 int main(){
     double timeIncrement = 0;
     double spaceIncrement = 0;
+    double endTime = 1;
+    double stockUpperBound = 4;
     incrementSetter(timeIncrement, spaceIncrement);
-    std::cout << timeIncrement << '\n';
-    std::cout << spaceIncrement;
+    int timeSteps = endTime/timeIncrement;
+    int stockSteps = stockUpperBound/spaceIncrement;
+    double SOLUTION_GRID[timeSteps][stockSteps]; // Sets the grid for our PDE to solve
+    std::cout << sizeof(SOLUTION_GRID);
     return 0;
 }
