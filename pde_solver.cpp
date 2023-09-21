3eb127a753866bf5e061139a25e2f62dcf1e19dc
Pradyumna Harish <harishpradyumna@gmail.com>
2023-09-21 14:19:43 +0400

    There still is a bug.
    Stability of solution is an issue for the BS PDE

--- pde_solver.cpp
+++ pde_solver.cpp
@@ -1,69 +1,62 @@
 #include <iostream>
 #include <cmath>
 
-
-double SPACE_STEP_SIZE = 0.005;
-double TIME_STEP_SIZE = std::sqrt(2*SPACE_STEP_SIZE); // Ensuring stability of results
-double END_TIME = 1;
-double STOCK_UPPER_BOUND = 4;
+double STEP_SIZE_SPACE = 0.05;
+double STEP_SIZE_TIME = std::sqrt(2*STEP_SIZE_SPACE); // ensures stability of solution
+double SPACE_UPPER_BOUND = 10;
+double SPACE_LOWER_BOUND = 0;
+double TIME_UPPER_BOUND = 10;
 double INTEREST = 0.01;
-double VOLATILITY = 1;
-int ROWS;
+double VOLATILITY_SQUARED = 1;
 int COLUMNS;
+int ROWS;
 
-
- // TODO: find a way to make the initial condition of the grid be what we want it to be
-int timeSteps(double endTime, double timeIncrement){
-    return static_cast<int>(endTime/timeIncrement);
-}
-
-int stockSteps(double stockUpperBound, double spaceIncrement){
-    return static_cast<int>(stockUpperBound/spaceIncrement);
-}
-
-void gridDimensions(
-    double endTime,
-    double stockUpperBound,
-    double timeIncrement,
-    double spaceIncrement,
-    int& rows,
-    int& columns){
-        int nRows = endTime/timeIncrement;
-        int nCols = stockUpperBound/spaceIncrement + 2*nRows; // to deal with the unbounded case
-        rows = nRows;
-        columns = nCols;
-}
-
-double payoff(double stock, double strike=0){
-    if (stock - strike > 0){
-        return stock - strike;
+void getGridDimensions(
+    double stepSizeSpace=STEP_SIZE_SPACE,
+    double stepSizeTime=STEP_SIZE_TIME,
+    double spaceUpperBound=SPACE_UPPER_BOUND,
+    double spaceLowerBound=SPACE_LOWER_BOUND,
+    double timeUpperBound=TIME_UPPER_BOUND,
+    int* columns=&COLUMNS,
+    int* rows=&ROWS
+    ){
+        double distance = spaceUpperBound - spaceLowerBound;
+        *columns = static_cast<int>(distance/stepSizeSpace);
+        *rows = static_cast<int>(timeUpperBound/stepSizeTime);
     }
+
+float initialValue(float spaceVal){
+    if (spaceVal - 5 > 0){
+        return spaceVal - 5;
+    } 
     return 0;
 }
 
-int main(){
-    gridDimensions(END_TIME, STOCK_UPPER_BOUND, TIME_STEP_SIZE, SPACE_STEP_SIZE, ROWS, COLUMNS);
-    double SOLUTION_GRID[ROWS][COLUMNS]; // Sets the grid for our PDE to solve
 
-    for (int i=0; i<COLUMNS; i++){ // initialising grid
-        SOLUTION_GRID[0][i] = payoff(-ROWS*SPACE_STEP_SIZE + i*SPACE_STEP_SIZE);
-    };
+
+int main(){
+    getGridDimensions();
+    double SOLUTION_GRID[ROWS][COLUMNS];
+    for (int i=1; i<COLUMNS; i++){ // initial condition
+        SOLUTION_GRID[0][i] = initialValue(i*STEP_SIZE_SPACE);
+    }
+    for (int i=0; i<ROWS; i++){
+        SOLUTION_GRID[i][0] = 0;
+        SOLUTION_GRID[i][COLUMNS-1] = 100;
+    }
 
     for (int timeStep=0; timeStep<ROWS-1; timeStep++){
-        for (int spaceStep=timeStep; spaceStep<COLUMNS-timeStep; spaceStep++){
+        for (int spaceStep=1; spaceStep<COLUMNS-1; spaceStep++){
+            double stockPrice = STEP_SIZE_SPACE*spaceStep;
             double currentValue = SOLUTION_GRID[timeStep][spaceStep];
-            double firstDerivative = (SOLUTION_GRID[timeStep][spaceStep+1] - SOLUTION_GRID[timeStep][spaceStep-1])/(2*TIME_STEP_SIZE);
-            double secondDerivative = (SOLUTION_GRID[timeStep][spaceStep+1] - 2*SOLUTION_GRID[timeStep][spaceStep]- SOLUTION_GRID[timeStep][spaceStep-1])/(TIME_STEP_SIZE*TIME_STEP_SIZE);
-            double stock = -ROWS*SPACE_STEP_SIZE + spaceStep*SPACE_STEP_SIZE;
-            SOLUTION_GRID[timeStep+1][spaceStep] = currentValue - SPACE_STEP_SIZE*(INTEREST*currentValue - INTEREST*stock*firstDerivative - 0.5*std::pow(VOLATILITY, 2)*std::pow(stock, 2)*secondDerivative); 
-        };
-    }; // This is the scheme to solving the PDE
-
-    for (int i=0; i < COLUMNS; i++){
-        std::cout << SOLUTION_GRID[ROWS-1][i] << '\n';
-    };
-
-
-
+            double firstDerivative = (SOLUTION_GRID[timeStep][spaceStep+1] - SOLUTION_GRID[timeStep][spaceStep-1])/(2*STEP_SIZE_TIME);
+            double secondDerivative = (SOLUTION_GRID[timeStep][spaceStep+1] - 2*SOLUTION_GRID[timeStep][spaceStep] + SOLUTION_GRID[timeStep][spaceStep-1])/(std::pow(STEP_SIZE_TIME,2));
+            double generator = INTEREST*currentValue - INTEREST*stockPrice*firstDerivative - 0.5*VOLATILITY_SQUARED*std::pow(stockPrice, 2)*secondDerivative;
+            SOLUTION_GRID[timeStep+1][spaceStep] = currentValue - STEP_SIZE_SPACE*generator; // ready to solve a generic problem
+        }
+    }
+    for (int i=0; i<ROWS; i++){
+        std::cout << SOLUTION_GRID[i][100] << '\n';
+    }
     return 0;
 }
