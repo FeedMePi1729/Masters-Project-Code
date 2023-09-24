f72c209fbf7706dffe360c36d361af85bd5c61c1
Pradyumna Harish <harishpradyumna@gmail.com>
2023-09-24 17:24:44 +0400

    Fixed the bug - can now solve BS PDE.
    Next step, make it compatible with processes.

--- pde_solver.py
+++ pde_solver.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-STEP_SIZE_SPACE = 0.05
+STEP_SIZE_SPACE = 0.01
 STEP_SIZE_TIME = np.sqrt(8*STEP_SIZE_SPACE)
 SPACE_UPPER_BOUND = 50
 SPACE_LOWER_BOUND = 0
@@ -13,7 +13,10 @@ ROWS = int(TIME_UPPER_BOUND/STEP_SIZE_TIME)
 SOLUTION_GRID = np.zeros((ROWS, COLUMNS))
 
 
-class Grid:
+class Grid: 
+    """
+    Is a class that can solve the Black-Scholes (or any generic PDE)
+    """
     def __init__(self, initialCondition):
         self.initialCondition = initialCondition
         self.nRows = int(TIME_UPPER_BOUND/STEP_SIZE_TIME)
@@ -44,8 +47,8 @@ class Grid:
         return self.grid[timeIndex-1][spaceIndex-1]
     
     def plotSolution(self, stockPrice):
-        time = np.arange(0, TIME_UPPER_BOUND, STEP_SIZE_TIME)
-        prices = [optionFairPrice(stockPrice, t) for t in time]
+        time = np.arange(0, TIME_UPPER_BOUND-2, STEP_SIZE_TIME)
+        prices = [self.optionFairPrice(stockPrice, t) for t in time]
         plt.plot(time, prices)
         plt.show()
         
@@ -54,38 +57,10 @@ def terminalCondition(spaceValue):
         return spaceValue - 5
     return 0
 
-def initialiseGrid(lowerBoundaryCondition=0, upperBoundaryCondition=95):
-    for i in range(ROWS):
-        SOLUTION_GRID[i][0] = lowerBoundaryCondition
-        SOLUTION_GRID[i][COLUMNS-1] = upperBoundaryCondition
-    
-    
-    for i in range(COLUMNS):
-        SOLUTION_GRID[0][i] = terminalCondition(i*STEP_SIZE_SPACE)
-
-
-def solvePDE():
-    for timeStep in range(ROWS-1):
-        for spaceStep in range(1, COLUMNS-1):
-            stockPrice = STEP_SIZE_SPACE*spaceStep
-            currentValue = SOLUTION_GRID[timeStep][spaceStep]
-            firstDerivative = (SOLUTION_GRID[timeStep][spaceStep+1] - SOLUTION_GRID[timeStep][spaceStep-1])/(2*STEP_SIZE_TIME)
-            secondDerivative = (SOLUTION_GRID[timeStep][spaceStep+1] - 2*SOLUTION_GRID[timeStep][spaceStep] + SOLUTION_GRID[timeStep][spaceStep-1])/(STEP_SIZE_TIME**2)
-            generator = INTEREST*currentValue - INTEREST*stockPrice*firstDerivative - 0.5*VOLATILITY_SQUARED*(stockPrice**2)*secondDerivative
-            SOLUTION_GRID[timeStep+1][spaceStep] = currentValue - STEP_SIZE_SPACE*generator
-
-def optionFairPrice(stockPrice, time):
-    spaceIndex = int(stockPrice/STEP_SIZE_SPACE)
-    timeIndex = int((TIME_UPPER_BOUND-time)/STEP_SIZE_TIME)
-    return SOLUTION_GRID[timeIndex-1][spaceIndex-1]
-
 def main():
-    x = Grid(initialCondition=terminalCondition)
+    x = Grid(terminalCondition)
     x.initialiseGrid()
     x.solvePDE()
-    stockPrice = np.arange(SPACE_LOWER_BOUND, SPACE_UPPER_BOUND, STEP_SIZE_SPACE)
-    price = [optionFairPrice(stock, 1) for stock in stockPrice]
-    plt.plot(stockPrice, price)
-    plt.show()
+    x.plotSolution(stockPrice=6)
 if __name__ == "__main__":
     main()
