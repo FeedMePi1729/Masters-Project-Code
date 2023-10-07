dd691d097bb7d867ebd8cae821249eaf573b960a
Pradyumna Harish <harishpradyumna@gmail.com>
2023-10-07 19:22:19 +0100

    created a place to stage everything
    Need to include dynamic pricing

---
+++ Current/python_code/fair_pricer.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from BlackScholes import BlackScholes
+from PathIndependentOption import *
+
+def main():
+    model = BlackScholes(10, 0.03, 1)
+    derivative = CallOption(2, 1000, 20)
+    print(derivative.monteCarloPricer(model, 10_000))
+
+if __name__ == "__main__":
+    main()
