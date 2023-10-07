6b106e92ade9f9cac8e21337320dba0fbe303b13
Pradyumna Harish <harishpradyumna@gmail.com>
2023-10-07 19:23:14 +0100

    Looking good so far - need to calculate the greeks
    Also need to create dynamic hedging

--- Current/python_code/PathIndependentOption.py
+++ Current/python_code/PathIndependentOption.py
@@ -1,7 +1,7 @@
 import numpy as np
 from BlackScholes import BlackScholes 
 
-class PathIndependentOption:
+class Derivative:
     def __init__(self, expiry_, steps_):
         self.expiry = expiry_
         self.steps = steps_ 
@@ -10,21 +10,26 @@ class PathIndependentOption:
         """
         This is a function that will be overriden when we use this base class for inheritance
         """
-        
         return 0
     
-    def monteCarloPricer(self, Model: BlackScholes, iterations):
+    def monteCarloPricer(self, Model: BlackScholes, iterations=int(1e+6)):
         value = 0
         for i in range(iterations):
             Model.generateSamplePath(self.expiry, self.steps)
             path = Model.getSamplePath()
             value = (i*value + self.payoff(path))/(i+1)
         return np.exp(-Model.interest*self.expiry)*value
-
-        
-               
     
-class CallOption(PathIndependentOption):
+    def arbitrageFreePrice(self, Model: BlackScholes, simulations, iterations):
+        aggregateSum = 0
+        value = 0
+        for i in range(simulations):
+            value = self.monteCarloPricer(Model, iterations)
+            aggregateSum += value
+        value /= simulations
+        return value
+                   
+class CallOption(Derivative):
     def __init__(self, expiry_, steps_, strike_):
         self.expiry = expiry_
         self.steps = steps_
