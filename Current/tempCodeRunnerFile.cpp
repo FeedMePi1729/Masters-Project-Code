0337d4f2c1ba771449a1afcb51deb714a7895fa7
Pradyumna Harish <harishpradyumna@gmail.com>
2023-10-07 15:56:48 +0100

    for testing whether classes work over certain projects
    Found the error using this

---
+++ Current/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+#include "Dog.h"
+
+int main(){
+    Dog Ruffles(13);
+    std::cout << Ruffles.getAge();
+}
---
+++ Current/test/Dog.cpp
@@ -0,0 +1,11 @@
+#include "Dog.h"
+
+Dog::Dog(int age_){age = age_;}
+
+int Dog::getAge(){
+    return age;
+}
+
+int main(){
+    return 0;
+}
